d51faed3949ea564e3b8fc7b187367c93127aa52
Lauri Kananen <lauri.kananen@solita.fi>
2020-12-01 14:48:27 +0200

    Second part Haskell solution created.

--- day1/Day1Solution.hs
+++ day1/Day1Solution.hs
@@ -2,10 +2,16 @@ module Day1Solution where
 
 import System.IO
 
--- Returns value pairs that sum up to target sum
-filterByTwo :: (Ord a, Num a) => [a] -> a -> [[a]]
-filterByTwo values target =
-     [[x, y] | x<-values, y<-values, x + y == target, x < y]
+-- Returns n values which have the n entries sum corresponding to the target.
+-- E.g. n=2 returns pairs     that sum up to the target sum.
+--      n=3 returns triplets  that sum up to the target sum.
+-- Note! Only implemented for n=[1..3]
+-- filterByN :: (Ord a1, Num a1, Num a, Eq a) => [a1] -> a1 -> a -> [[a1]]
+filterByN values target n
+    | n == 1 = [[x]       | x<-values,                       x == target]
+    | n == 2 = [[x, y]    | x<-values, y<-values,            x + y == target,     x < y]
+    | n == 3 = [[x, y, z] | x<-values, y<-values, z<-values, x + y + z == target, x < y, y < z]
+filterByN [] _ n = []
 
 -- Converts a string with endline chars to an integer
 -- E.g. "12\n" -> 12
@@ -14,15 +20,19 @@ strToInt xs = read [x | x<-xs, not (elem x "\n\r")] :: Integer
 
 -- Uses list comprehension to prepare the data as a list of integers. The data gets run through the filter
 -- and then the same preparation steps (conversions) get undone.
-filterData :: String -> String
-filterData d = unlines [show l | l<-(filterByTwo [strToInt l | l<-(lines d)] 2020)]
+filterData :: (Num a, Eq a) => String -> Integer -> a -> String
+filterData d target n = unlines [show l | l<-(filterByN [strToInt l | l<-(lines d)] target n)]
 
 
 main :: IO()
 main = do
     fileHandle <- openFile "day1RawDataDump.txt" ReadMode
     contents <- hGetContents fileHandle
-    -- putStr contents
 
-    putStrLn $ filterData contents
+    putStrLn "Solution pairs:"
+    putStrLn $ filterData contents 2020 2
+
+    putStrLn "Solution triplets:"
+    putStrLn $ filterData contents 2020 3
+
     hClose fileHandle
--- day1/Day1SolutionTest.hs
+++ day1/Day1SolutionTest.hs
@@ -10,12 +10,17 @@ twoDigitExampleData = fmap (filter (\x -> length x > 1 && length x < 3) . lines)
 processedExampleData = [Day1Solution.strToInt i | i<-lines rawNums]
 
 test1Expected = [1,23,45,6,78,9]
-test1 = TestCase (assertEqual "Raw string to integer conversion" processedExampleData test1Expected)
+test1 = TestCase (assertEqual "Raw string to integer conversion" test1Expected processedExampleData)
 
 test2Input = "1010\n23\n61\n1010\n2019\n1\n2020\n0"
 test2Expected = "[1,2019]\n[0,2020]\n"
-test2 = TestCase (assertEqual "Custom data test" (Day1Solution.filterData test2Input) test2Expected )
+test2 = TestCase (assertEqual "Custom data test" test2Expected $ Day1Solution.filterData test2Input 2020 2)
+
+test3Input = "1010\n23\n61\n1010\n2019\n1\n2020\n0"
+test3Expected = "[0,1,2019]\n"
+test3 = TestCase (assertEqual "Custom data test" test3Expected $ Day1Solution.filterData test3Input 2020 3)
 
 tests = TestList[TestLabel "str2int" test1,
-                 TestLabel "Filter test for custom input" test2
+                 TestLabel "Pair filter test for custom input" test2,
+                 TestLabel "Triplet filter test for custom input" test3
                  ]
